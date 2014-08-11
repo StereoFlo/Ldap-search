600630f07d597702dd36c50eb25471105565a1ed
lanuser <lanuser@lanuser.ru>
2014-08-11 09:32:08 +0400

    comments

--- ldap.php
+++ ldap.php
@@ -1,7 +1,8 @@
 <?php 
 
-// ингда ну прсто необходимо использовать
-//error_reporting(0);
+/**
+* The class for auth with an active directory services.
+*/
 
 class Ldap {
 
@@ -11,80 +12,116 @@ class Ldap {
     public $basepass;
     public $connection;
 
+	/**
+	* Constructor method
+	* @param array $params is an array contains parameters to connection
+	* 
+	* @return
+	*/
+	
     function __construct ($params = array())
     {
-	$this->ldapserver = $params['ldapserver'];
-	$this->ldapport = $params['ldapport'];
-	$this->basedn = $params['basedn'];
-	$this->basepass = $params['basepass'];
-	
-	$this->connection = $this->connect($this->ldapserver, $this->ldapport);
-	$this->bind();
+		$this->ldapserver = $params['ldapserver'];
+		$this->ldapport = $params['ldapport'];
+		$this->basedn = $params['basedn'];
+		$this->basepass = $params['basepass'];
+		
+		$this->connection = $this->connect($this->ldapserver, $this->ldapport);
+		$this->bind();
     }
+        
+    /**
+	* private method for connect with a AD server
+	* @param string $server Need to contain an ip address of a domain controller.
+	* @param string $port Need to contain a port of a domain controller.
+	* 
+	* @return bool
+	*/
     
-    //Использование:  $connection = $this->ldap->connect("127.0.250.10","389");
     private function connect($server, $port)
     {
-	$connection = ldap_connect($server,$port);	
-	ldap_set_option($connection, LDAP_OPT_PROTOCOL_VERSION, 3);
-	return $connection;
-    }
-    //Использование $test = $this->ldap->bind($connection,'user@domain.ru','you_password');
-    //возвращает: 1 - ок; 0 - что-то не так
-    private function bind()
-    {
-	$bind = ldap_bind($this->connection, $this->basedn , $this->basepass);
-	if (!empty($bind))
-	{
-	    return 1;
-	}
-	else
-	{
-	    return 0;
-	}
-    }
-    //использование
-    //$sb = 'OU=Пользователи и компьютеры,DC=megafon,DC=ru';
-    //$filter = "(&(objectClass=user)(sAMAccountName=login))";
-    //$attr = array('samaccountname');
-    //$test = $this->ldap->search($connection, $sb, $filter, $attr);
-    //возвратит массив пользователя, если данные есть и в массиве один элемент и int 0, если ни чего не найдено или найдено более одной записи.
-    function search($searchdn, $filter, $attributes = array())
-    {
-	$sr = ldap_search($this->connection, $searchdn, $filter, $attributes);
-	if ($sr)
-	{
-	    $info = ldap_get_entries($this->connection, $sr);
-	    if ($info["count"] == 1)
-	    {
-                return $info;
-	    }
-	    else
-	    {
-		return 0;
-	    }
-        }    
-    }
-
-    // поиск пользователей. работает как и $this->search, но может возвращать не только один элемент
-    function search2($searchdn, $filter, $attributes = array())
-    {
-	$sr = ldap_search($this->connection, $searchdn, $filter, $attributes);
-	if ($sr)
-	{
-	    $info = ldap_get_entries($this->connection, $sr);
-            return $info;
-        }
-	else
-	{
-	    return 0;
-	}
+		$connection = ldap_connect($server, $port);	
+		ldap_set_option($connection, LDAP_OPT_PROTOCOL_VERSION, 3);
+		return $connection;
     }
     
-    // если вы нуждаетесь в закрытии соединения
-    function close($connection)
+    /**
+	* private method for bind a domain controller. Return integer, if all good is 1, if something wrong is 0
+	* 
+	* @return ineger
+	*/
+    
+    private function bind()
     {
-	ldap_close($connection);
+		$bind = ldap_bind($this->connection, $this->basedn , $this->basepass);
+		if (!empty($bind))
+		{
+		    return 1;
+		}
+		else
+		{
+		    return 0;
+		}
+    }
+    
+    /**
+	* Search public method, return first and one found entry
+	* @param string $searchdn is a search path, an example: 'OU=РџРѕР»СЊР·РѕРІР°С‚РµР»Рё Рё РєРѕРјРїСЊСЋС‚РµСЂС‹,DC=megafon,DC=ru'
+	* @param string $filter an AD filter, an example: "(&(objectClass=user)(sAMAccountName=login))"
+	* @param array $attributes contains an active directory attributes.
+	* 
+	* @return array if results exists
+	*/
+    
+    public function search($searchdn, $filter, $attributes = array())
+    {
+		$sr = ldap_search($this->connection, $searchdn, $filter, $attributes);
+		if ($sr)
+		{
+		    $info = ldap_get_entries($this->connection, $sr);
+		    if ($info["count"] == 1)
+		    {
+	                return $info;
+		    }
+		    else
+		    {
+				return 0;
+		    }
+	    }    
+    }
+
+    /**
+	* Search public method. distinction with the $this->search() can return more than 1 result
+	* @param string $searchdn is a search path, an example: 'OU=РџРѕР»СЊР·РѕРІР°С‚РµР»Рё Рё РєРѕРјРїСЊСЋС‚РµСЂС‹,DC=megafon,DC=ru'
+	* @param string $filter an AD filter, an example: "(&(objectClass=user)(sAMAccountName=login))"
+	* @param array $attributes contains an active directory attributes.
+	* 
+	* @return array if results exists
+	*/
+	
+    public function search2($searchdn, $filter, $attributes = array())
+    {
+		$sr = ldap_search($this->connection, $searchdn, $filter, $attributes);
+		if ($sr)
+		{
+		    $info = ldap_get_entries($this->connection, $sr);
+	        return $info;
+	    }
+		else
+		{
+		    return 0;
+		}
+    }
+    
+    /**
+	* The close method
+	* @param undefined $connection
+	* 
+	* @return
+	*/
+    public function close($connection)
+    {
+		ldap_close($connection);
     }
 
 }
